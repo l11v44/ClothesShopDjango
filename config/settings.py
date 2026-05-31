beaeb9b26a8108baf5880e218099822e304ca1e7
lav4 <vlad2511k@gmail.com>
2026-05-31 17:55:44 +0300

    67

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-38svf^k=h3#6#5y)bx-@4rxwjypqdy)d+q@*hg27y@rtazdi*x'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = []
